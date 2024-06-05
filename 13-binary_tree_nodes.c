1f6eb08810f23827de786fb5d8cdf46c1fab422b
Dennis Miring'u <dennismiringu03@gmail.com>
2024-06-05 17:19:38 +0300

    first 15 files to see if I have done it well

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+*
+**/
+
+size_t binary_trees_nodes(const binary_tree_t *tree)
+{
+    size_t nodes = 0;
+    if (tree)
+    {
+        nodes += (tree->left || tree->right) ? 1 : 0;
+        nodes += binary_tree_nodes(tree->left);
+        nodes += binary_tree_nodes(tree->right);
+    }
+    return (nodes)
+}
